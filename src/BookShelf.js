5fb9d8b4280f10fdb9baeeb64485dc69f9764bab
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2018-12-15 12:21:44 -0200

    refactor: Create BookShelf container

---
+++ src/BookShelf.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Book from './Book';
+
+const BookShelf = props => {
+  const { books, title } = props;
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map(book => (
+            <li>
+              <Book book={book} />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default BookShelf;

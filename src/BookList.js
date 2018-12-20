f48c64d583f40d79295583421284af815d922505
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2018-12-20 01:05:00 -0200

    refactor: DRY out BookList component

--- src/BookList.js
+++ src/BookList.js
@@ -5,6 +5,20 @@ import { Link } from 'react-router-dom';
 class BookList extends Component {
   render() {
     const { onBookShelfChange, books } = this.props;
+    const bookShelves = [
+      {
+        title: "Currently Reading",
+        shelf: "currentlyReading",
+      },
+      {
+        title: "Want to Read",
+        shelf: "wantToRead",
+      },
+      {
+        title: "Read",
+        shelf: "read",
+      }
+    ];
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -12,21 +26,14 @@ class BookList extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <BookShelf
-              title={"Currently Reading"}
-              books={books.filter(book => book.shelf === "currentlyReading")}
-              onBookShelfChange={onBookShelfChange}
-            />
-            <BookShelf
-              title={"Want to Read"}
-              books={books.filter(book => book.shelf === "wantToRead")}
-              onBookShelfChange={onBookShelfChange}
-            />
-            <BookShelf
-              title={"Read"}
-              books={books.filter(book => book.shelf === "read")}
-              onBookShelfChange={onBookShelfChange}
-            />
+            {bookShelves.map(bookShelf => (
+              <BookShelf
+                key={bookShelf.shelf}
+                title={bookShelf.title}
+                books={books.filter(book => book.shelf === bookShelf.shelf)}
+                onBookShelfChange={onBookShelfChange}
+              />
+            ))}
           </div>
         </div>
         <Link to={'/search'}>
